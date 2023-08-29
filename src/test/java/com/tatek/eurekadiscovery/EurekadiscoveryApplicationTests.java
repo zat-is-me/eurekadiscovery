1bd4bde6d166688680a010d62beeb7e3acf55312
Tatek <tatek.ahmed443@gmail.com>
2023-08-29 13:31:31 -0500

    Sharing to the world

---
+++ src/test/java/com/tatek/eurekadiscovery/EurekadiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatek.eurekadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekadiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
